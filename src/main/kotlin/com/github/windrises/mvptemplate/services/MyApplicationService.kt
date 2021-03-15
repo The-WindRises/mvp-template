24811a0af8dd4e5b7e96af73d259d158098e8b98
GitHub Action <action@github.com>
2021-03-15 07:43:42 +0000

    Template cleanup

--- src/main/kotlin/com/github/windrises/mvptemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/windrises/mvptemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.windrises.mvptemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.windrises.mvptemplate.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/windrises/mvptemplate/services/MyProjectService.kt
+++ src/main/kotlin/com/github/windrises/mvptemplate/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.windrises.mvptemplate.services
 
+import com.github.windrises.mvptemplate.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
